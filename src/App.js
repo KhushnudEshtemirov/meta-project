f333eaefbf52b48d2208e0a21f38d5bb83476a43
Khushnud Eshtemirov <khushnudeshtemirov@gmail.com>
2024-01-09 18:07:48 +0500

    Created simple calculator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [result, setResult] = useState(0);
+  const [value, setValue] = useState("");
+
+  const handleAdd = () => {
+    setResult((prev) => prev + parseInt(value));
+    setValue("");
+  };
+
+  const handleSubtract = () => {
+    setResult((prev) => prev - value);
+    setValue("");
+  };
+
+  const handleMultiply = () => {
+    setResult((prev) => prev * value);
+    setValue("");
+  };
+
+  const handleDivide = () => {
+    setResult((prev) => prev / value);
+    setValue("");
+  };
+
+  const handleChange = (val) => {
+    setValue(val);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Simplest Working Calculator</h2>
+      <p>{result}</p>
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => handleChange(e.target.value)}
+      />
+      <div className="buttons">
+        <button onClick={handleAdd}>Add</button>
+        <button onClick={handleSubtract}>Subtract</button>
+        <button onClick={handleMultiply}>Multiply</button>
+        <button onClick={handleDivide}>Divide</button>
+        <button onClick={() => setValue("")}>Reset input</button>
+        <button onClick={() => setResult(0)}>Reset result</button>
+      </div>
     </div>
   );
 }
