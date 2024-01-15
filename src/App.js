5416ddf915008afc37f4451dec9e93b7543d19c0
Khushnud Eshtemirov <khushnudeshtemirov@gmail.com>
2024-01-15 22:46:52 +0500

    Updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
+import { BiLogoGmail } from "react-icons/bi";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { BsMedium } from "react-icons/bs";
+import { FaStackOverflow } from "react-icons/fa";
 
 function App() {
+  const links = [
+    {
+      id: 1,
+      label: <BiLogoGmail />,
+      url: "https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox",
+    },
+    {
+      id: 2,
+      label: <FaGithub />,
+      url: "https://github.com/KhushnudEshtemirov",
+    },
+    {
+      id: 3,
+      label: <FaLinkedin />,
+      url: "https://www.linkedin.com/in/khushnud-eshtemirov/",
+    },
+    {
+      id: 4,
+      label: <BsMedium />,
+      url: "https://medium.com/@khushnudeshtemirov",
+    },
+    {
+      id: 5,
+      label: <FaStackOverflow />,
+      url: "https://stackoverflow.com/users/17287535/khushnud-eshtemirov",
+    },
+  ];
+
   const handleSubmit = (e) => {
     e.preventDefault();
   };
@@ -10,26 +43,18 @@ function App() {
     <div className="App">
       <header>
         <ul>
-          <li>
-            <a href="/">Logo1</a>
-          </li>
-          <li>
-            <a href="/">Logo2</a>
-          </li>
-          <li>
-            <a href="/">Logo3</a>
-          </li>
-          <li>
-            <a href="/">Logo4</a>
-          </li>
-          <li>
-            <a href="/">Logo5</a>
-          </li>
+          {links.map((link) => (
+            <li>
+              <a target="_blank" href={link.url}>
+                {link.label}
+              </a>
+            </li>
+          ))}
           <li className="right">
-            <a href="/">Projects</a>
+            <a href="#projects">Projects</a>
           </li>
           <li>
-            <a href="/">Contact Me</a>
+            <a href="#contact">Contact Me</a>
           </li>
         </ul>
       </header>
@@ -41,43 +66,46 @@ function App() {
           />
           <p>Hello, I am Khushud</p>
         </div>
-        <div className="job">A frontend developer in React</div>
+        <div className="job">A frontend developer specialised in React</div>
       </div>
-      <div className="projects">
-        <div className="project">
-          <img src="https://www.liquidplanner.com/wp-content/uploads/2019/04/HiRes-17.jpg" />
-          <div className="details">
-            <h3>My Project 1</h3>
-            <p>More text</p>
-            <a href="#">See more</a>
+      <div className="projects" id="projects">
+        <h1>Featured Projects</h1>
+        <div className="inner">
+          <div className="project">
+            <img src="https://www.liquidplanner.com/wp-content/uploads/2019/04/HiRes-17.jpg" />
+            <div className="details">
+              <h3>My Project 1</h3>
+              <p>More text</p>
+              <a href="#">See more</a>
+            </div>
           </div>
-        </div>
-        <div className="project">
-          <img src="https://www.simplilearn.com/ice9/free_resources_article_thumb/What_Is_a_Project.jpg" />
-          <div className="details">
-            <h3>My Project 2</h3>
-            <p>More text</p>
-            <a href="#">See more</a>
+          <div className="project">
+            <img src="https://www.simplilearn.com/ice9/free_resources_article_thumb/What_Is_a_Project.jpg" />
+            <div className="details">
+              <h3>My Project 2</h3>
+              <p>More text</p>
+              <a href="#">See more</a>
+            </div>
           </div>
-        </div>
-        <div className="project">
-          <img src="https://graduate.northeastern.edu/resources/wp-content/uploads/sites/4/2017/07/Project-Management-Skills-HERO.jpg" />
-          <div className="details">
-            <h3>My Project 3</h3>
-            <p>More text</p>
-            <a href="#">See more</a>
+          <div className="project">
+            <img src="https://graduate.northeastern.edu/resources/wp-content/uploads/sites/4/2017/07/Project-Management-Skills-HERO.jpg" />
+            <div className="details">
+              <h3>My Project 3</h3>
+              <p>More text</p>
+              <a href="#">See more</a>
+            </div>
           </div>
-        </div>
-        <div className="project">
-          <img src="https://cdn-bobah.nitrocdn.com/mDxsknkPRwKnnzdIpDlNbqFeHfgHfCqS/assets/images/optimized/rev-8879379/www.eiresystems.com/wp-content/uploads/Team-members-having-a-meeting..jpg" />
-          <div className="details">
-            <h3>My Project 4</h3>
-            <p>More text</p>
-            <a href="#">See more</a>
+          <div className="project">
+            <img src="https://cdn-bobah.nitrocdn.com/mDxsknkPRwKnnzdIpDlNbqFeHfgHfCqS/assets/images/optimized/rev-8879379/www.eiresystems.com/wp-content/uploads/Team-members-having-a-meeting..jpg" />
+            <div className="details">
+              <h3>My Project 4</h3>
+              <p>More text</p>
+              <a href="#">See more</a>
+            </div>
           </div>
         </div>
       </div>
-      <div className="contact">
+      <div className="contact" id="contact">
         <form onSubmit={handleSubmit}>
           <h2>Contact Me</h2>
           <label>Name</label>
